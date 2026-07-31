3a29c2356c73517e167fc9906ffa19d45d7d4b77
Ebrwa <ebrahimbileh21@gmail.com>
2026-07-31 20:35:30 +0300

    Finalize RAG implementation

--- main.py
+++ main.py
@@ -8,9 +8,7 @@ from groq import Groq
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain_core.documents import Document
-
-
+from langchain_core.documents import Docum
 INDEX_PATH = "faiss_index"
 CSV_PATH = "human_rights_links-2.csv"
 MODEL_NAME = "llama-3.3-70b-versatile"
